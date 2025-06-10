385f8b92a281818a84f61b081d73d81452adc9a8
Arthur Pin <arthur_pin@usp.br>
2025-06-10 16:34:28 -0300

    Email.cs: create Email record separated from main code

---
+++ Email.cs
@@ -0,0 +1,47 @@
+using System.Net.Mail;
+using System.Net;
+
+/// <summary>
+/// Exception thrown when an error occurs while trying to send an email.
+/// </summary>
+public class SendEmailException : Exception {
+	public SendEmailException(string? message) : base(message) { }
+};
+
+/// <summary>
+/// Represents the email sending service.
+/// </summary>
+/// <param name="client">The SmtpClient configured to send emails.</param>
+/// <param name="user">The sender's email address.</param>
+public record Email (
+	SmtpClient client,
+	string user
+) {
+	/// <summary>
+	/// Constructor that initializes the email service from a configuration object.
+	/// </summary>
+	/// <param name="config">The config with email service settings.</param>
+	public Email(Config config) : this(
+		new SmtpClient(config.SmtpServer, config.SmtpPort){
+			Credentials = new NetworkCredential(config.SmtpUser, config.SmtpPassword),	
+			EnableSsl = true
+		},
+		config.SmtpUser
+	){}
+
+	/// <summary>
+	/// Sends an email.
+	/// </summary>
+	/// <param name="targetEmail">The recipient's email address.</param>
+	/// <param name="subject">The email's subject.</param>
+	/// <param name="body">The email's body.</param>
+	public void Send(string targetEmail, string subject, string body){
+		try {
+			var message = new MailMessage(user, targetEmail, subject, body);
+
+			client.Send(message);
+		} catch {
+			throw new SendEmailException("Nao foi possivel enviar o email.");
+		}
+	}
+}
