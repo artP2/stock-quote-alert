d59078fb3029f2f840bcad9acf55ccdf3ba3d9f1
Arthur Pin <arthur_pin@usp.br>
2025-06-10 11:48:25 -0300

    load Config from config.json Creates the Config record for the email service configuration and parses it from config.json. Loads the config in the Main function. Adds a example config.json.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,38 @@
-﻿/// <summary>
+using System.Text.Json;
+
+/// <summary>
+/// Represents the structure of the application's configuration data.
+/// </summary>
+/// <param name="TargetEmail">The email that will receive the alerts.</param>
+/// <param name="SmtpServer">The SMTP server address.</param>
+/// <param name="SmtpPort">The SMTP server port.</param>
+/// <param name="SmtpUser">The username for SMTP authentication.</param>
+/// <param name="SmtpPassword">The password for SMTP authentication.</param>
+public record Config (
+	string TargetEmail,
+	string SmtpServer,
+	int SmtpPort,
+	string SmtpUser,
+	string SmtpPassword
+) {
+	/// <summary>
+	/// Loads the settings from the 'config.json' file.
+	/// </summary>
+	/// <returns>A Config instance with data from the file.</returns>
+	static public Config Load(){
+		try {
+		var json = File.ReadAllText("config.json");
+		return JsonSerializer.Deserialize<Config>(json)
+			?? throw new JsonException();
+		} catch (FileNotFoundException) {
+			throw new Exception("ERRO: arquivo de configuraçao 'config.json' nao encontrado.");
+		} catch (JsonException) {
+			throw new Exception("ERRO: Configuraçao invalida.");
+		}
+	}
+}
+
+/// <summary>
 /// Exception thrown when arguments are invalid.
 /// </summary>
 public class ArgsParseException : Exception {
@@ -43,12 +77,17 @@ public class StockQuoteAlert {
 	/// <param name="args">Command-line arguments: [stock_name] [sell_price] [buy_price]</param>
 	public static void Main(string[] args) {
 		try {
+			Console.WriteLine("Carregando configuraçao...");
+			Config config = Config.Load();
+			Console.WriteLine("Configuraçao carregada!");
+			Console.WriteLine($"Email de destino: {config.TargetEmail}");
 			Stock stock = Stock.Parse(args);
 			Console.WriteLine(
 				$"Ativo: {stock.StockName}\nVenda em: {stock.SellPrice}\nCompra em: {stock.BuyPrice}"
 			);
 		} catch (Exception e) {
 			Console.WriteLine(e.Message);
+			Environment.Exit(1);
 		}
 	}
 } 
