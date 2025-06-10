d139a0a20559c751549dc82a8043eef1df50e435
Arthur Pin <arthur_pin@usp.br>
2025-06-10 12:07:12 -0300

    add SendEmail function creates the email service and a function to send an email.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using System.Text.Json;
+using System.Net.Mail;
+using System.Net;
 
 /// <summary>
 /// Represents the structure of the application's configuration data.
@@ -68,9 +70,35 @@ public record Stock (
 };
 
 /// <summary>
+/// Exception thrown when an error occurs while trying to send an email.
+/// </summary>
+public class SendEmailException : Exception {
+	public SendEmailException(string? message) : base(message) { }
+};
+
+/// <summary>
 /// Main class that orchestrates stock monitoring.
 /// </summary>
 public class StockQuoteAlert {
+	/// <summary>
+	/// Sends an email.
+	/// </summary>
+	/// <param name="config">The config with email service data.</param>
+	/// <param name="subject">The email's subject.</param>
+	/// <param name="body">The email's body.</param>
+	void SendEmail(Config config, string subject, string body){
+		try {
+			var client = new SmtpClient(config.SmtpServer, config.SmtpPort){
+				Credentials = new NetworkCredential(config.SmtpUser, config.SmtpPassword)	
+			};
+			var message = new MailMessage(config.SmtpUser, config.TargetEmail, subject, body);
+
+			client.Send(message);
+		} catch {
+			throw new SendEmailException("ERRO: Nao foi possivel enviar o email.");
+		}
+	}
+
 	/// <summary>
 	/// Main entry method of the program.
 	/// </summary>
@@ -85,6 +113,8 @@ public class StockQuoteAlert {
 			Console.WriteLine(
 				$"Ativo: {stock.StockName}\nVenda em: {stock.SellPrice}\nCompra em: {stock.BuyPrice}"
 			);
+		} catch (SendEmailException e) {
+			Console.WriteLine(e.Message);
 		} catch (Exception e) {
 			Console.WriteLine(e.Message);
 			Environment.Exit(1);
